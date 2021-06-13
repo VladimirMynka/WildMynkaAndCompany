6dff68226b32a2e20fb1416a58e08feeceb6659b
VladimirMynka <vladimirmynka34821@gmail.com>
2021-06-13 15:35:00 +0700

    Добавил ещё менюшек для сохранений и загрузок, они пока не функционирующие, но они есть. Создал префабы кнопок. Исправил функции в Saver, игрок теперь сохраняется полностью

--- Assets/Scripts/Global/Saver/PlayerSaver.cs
+++ Assets/Scripts/Global/Saver/PlayerSaver.cs
@@ -25,59 +25,6 @@ public class PlayerSaver : Saver
         Push();
     }
 
-    private void SaveHealth()
-    {
-        var health = GetComponent<Health>();
-        Put(health.current);
-        Put(health.maxHealth);
-        Put(health.regeneration);
-    }
-
-    private void SaveCharacteristics()
-    {
-        var characteristics = GetComponent<Characteristics>();
-        Put(characteristics.attack);
-        Put(characteristics.power);
-        Put(characteristics.protection);
-        Put(characteristics.attackMagic);
-        Put(characteristics.selfMagic);
-        Put(characteristics.strangeMagic);
-        Put(characteristics.camouflage);
-    }
-
-    private void SaveInventory()
-    {
-        var inventory = GetComponent<Inventory>();
-        
-        Put(inventory.weapons.Count);
-        foreach (var weapon in inventory.weapons)
-            Put(GetPath(weapon));
-        
-        Put(inventory.spells.Count);
-        foreach (var spell in inventory.spells)
-            Put(GetPath(spell));
-        
-        Put(inventory.keys.Count);
-        foreach (var key in inventory.keys)
-            Put(GetPath(key));
-        
-        Put(inventory.others.Count);
-        foreach (var other in inventory.others)
-            Put(GetPath(other));
-    }
-
-    private void SavePlayerAttack()
-    {
-        var attack = GetComponent<PlayerAttack>();
-        Put(attack.index);
-    }
-
-    private void SavePlayerCastSpell()
-    {
-        var castSpell = GetComponent<PlayerCastSpell>();
-        Put(castSpell.index);
-    }
-
     public override void Load(string saveName)
     {
         savedName = gameObject.name;
@@ -90,58 +37,5 @@ public class PlayerSaver : Saver
         LoadInventory();
         LoadPlayerAttack();
         LoadPlayerCastSpell();
-    }
-
-    private void LoadHealth()
-    {
-        var health = GetComponent<Health>();
-        health.current = NextFloat();
-        health.maxHealth = NextFloat();
-        health.regeneration = NextFloat();
-    }
-    private void LoadCharacteristics()
-    {
-        var characteristics = GetComponent<Characteristics>();
-        characteristics.attack = NextFloat();
-        characteristics.power = NextFloat();
-        characteristics.protection = NextFloat();
-        characteristics.attackMagic = NextFloat();
-        characteristics.selfMagic = NextFloat();
-        characteristics.strangeMagic = NextFloat();
-        characteristics.camouflage = NextFloat();
-    }
-    private void LoadInventory()
-    {
-        var inventory = GetComponent<Inventory>();
-
-        int weaponsCount = NextInt();
-        inventory.weapons = new List<GameObject>(weaponsCount);
-        for(int i = 0; i < weaponsCount; i++)
-            inventory.weapons.Add(Resources.Load<GameObject>(NextString()));
-        
-        int spellsCount = NextInt();
-        inventory.spells = new List<GameObject>(spellsCount);
-        for(int i = 0; i < spellsCount; i++)
-            inventory.spells.Add(Resources.Load<GameObject>(NextString()));
-
-        int keysCount = NextInt();
-        inventory.keys = new List<GameObject>(keysCount);
-        for(int i = 0; i < keysCount; i++)
-            inventory.keys.Add(Resources.Load<GameObject>(NextString()));
-
-        int othersCount = NextInt();
-        inventory.others = new List<GameObject>(othersCount);
-        for(int i = 0; i < othersCount; i++)
-            inventory.others.Add(Resources.Load<GameObject>(NextString()));
-    }
-    private void LoadPlayerAttack()
-    {
-        var attack = GetComponent<PlayerAttack>();
-        attack.index = NextInt();
-    }
-    private void LoadPlayerCastSpell()
-    {
-        var castSpell = GetComponent<PlayerCastSpell>();
-        castSpell.index = NextInt();
     }
 }
--- Assets/Scripts/Global/Saver/Saver.cs
+++ Assets/Scripts/Global/Saver/Saver.cs
@@ -48,7 +48,33 @@ public abstract class Saver : MonoBehaviour
     protected string GetString() =>
         PlayerPrefs.GetString(savedName);
     
-    
+
+    protected static float NextFloat()
+    {
+        return matcher.nextFloat();
+    }
+
+    protected static int NextInt()
+    {
+        return matcher.nextInt();
+    }
+
+    protected static string NextString()
+    {
+        return matcher.nextString();
+    }
+
+    protected static string GetPath(UnityEngine.Object obj)
+    {
+        // Path's format: Assets/Recourses/.../file.smth
+        // We have to remove "Assets/Recourses/" and ".smth" for Unity to correctly load file
+        string fullPath = AssetDatabase.GetAssetPath(obj);
+
+        // "Assets/Recourses/".length == 17
+        string correctPath = fullPath.Substring(17, fullPath.LastIndexOf('.') - 17);
+        return correctPath;
+    }
+
     protected void SaveTransform()
     {
         var temp = transform;
@@ -66,7 +92,6 @@ public abstract class Saver : MonoBehaviour
         Put(rotation.z);
         Put(rotation.w);
     }
-    
     protected void LoadTransform()
     {
         var transformLocal = transform;
@@ -76,22 +101,6 @@ public abstract class Saver : MonoBehaviour
         transformLocal.localRotation = new Quaternion(NextFloat(), NextFloat(), NextFloat(), NextFloat());
     }
 
-    protected static float NextFloat()
-    {
-        return matcher.nextFloat();
-    }
-
-    protected static int NextInt()
-    {
-        return matcher.nextInt();
-    }
-
-    protected static string NextString()
-    {
-        return matcher.nextString();
-    }
-
-
     protected void SaveSprite()
     {
         var sprite = GetComponent<SpriteRenderer>();
@@ -99,23 +108,124 @@ public abstract class Saver : MonoBehaviour
 
         Put(path);
         Put(sprite.sortingOrder);
-        
-    }
-
-    protected static string GetPath(UnityEngine.Object obj)
-    {
-        // Path's format: Assets/Recourses/.../file.smth
-        // We have to remove "Assets/Recourses/" and ".smth" for Unity to correctly load file
-        string fullPath = AssetDatabase.GetAssetPath(obj);
-
-        // "Assets/Recourses/".length == 17
-        string correctPath = fullPath.Substring(17, fullPath.LastIndexOf('.') - 17);
-        return correctPath;
     }
     protected void LoadSprite()
     {
-        var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        if(spriteRenderer == null) spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
         spriteRenderer.sprite = Resources.Load<Sprite>(NextString());
         spriteRenderer.sortingOrder = NextInt();
     }
+
+    protected void SaveHealth()
+    {
+        var health = gameObject.GetComponent<Health>();
+        Put(health.current);
+        Put(health.maxHealth);
+        Put(health.regeneration);
+    }
+    protected void LoadHealth()
+    {
+        var health = gameObject.GetComponent<Health>();
+        if(health == null) health = gameObject.AddComponent<Health>();
+        health.current = NextFloat();
+        health.maxHealth = NextFloat();
+        health.regeneration = NextFloat();
+    }
+
+    protected void SaveCharacteristics()
+    {
+        var characteristics = gameObject.GetComponent<Characteristics>();
+        Put(characteristics.attack);
+        Put(characteristics.power);
+        Put(characteristics.protection);
+        Put(characteristics.attackMagic);
+        Put(characteristics.selfMagic);
+        Put(characteristics.strangeMagic);
+        Put(characteristics.camouflage);
+    }
+    protected void LoadCharacteristics()
+    {
+        var characteristics = gameObject.GetComponent<Characteristics>();
+        if(characteristics == null) characteristics = gameObject.AddComponent<Characteristics>();
+        characteristics.attack = NextFloat();
+        characteristics.power = NextFloat();
+        characteristics.protection = NextFloat();
+        characteristics.attackMagic = NextFloat();
+        characteristics.selfMagic = NextFloat();
+        characteristics.strangeMagic = NextFloat();
+        characteristics.camouflage = NextFloat();
+    }
+
+    protected void SaveInventory()
+    {
+        var inventory = gameObject.GetComponent<Inventory>();
+        
+        Put(inventory.weapons.Count);
+        foreach (var weapon in inventory.weapons)
+            Put(GetPath(weapon));
+        
+        Put(inventory.spells.Count);
+        foreach (var spell in inventory.spells)
+            Put(GetPath(spell));
+        
+        Put(inventory.keys.Count);
+        foreach (var key in inventory.keys)
+            Put(GetPath(key));
+        
+        Put(inventory.others.Count);
+        foreach (var other in inventory.others)
+            Put(GetPath(other));
+    }
+    protected void LoadInventory()
+    {
+        var inventory = gameObject.GetComponent<Inventory>();
+        if (inventory == null) inventory = gameObject.AddComponent<Inventory>();
+
+        int weaponsCount = NextInt();
+        inventory.weapons = new List<GameObject>(weaponsCount);
+        for(int i = 0; i < weaponsCount; i++)
+            inventory.weapons.Add(Resources.Load<GameObject>(NextString()));
+        
+        int spellsCount = NextInt();
+        inventory.spells = new List<GameObject>(spellsCount);
+        for(int i = 0; i < spellsCount; i++)
+            inventory.spells.Add(Resources.Load<GameObject>(NextString()));
+
+        int keysCount = NextInt();
+        inventory.keys = new List<GameObject>(keysCount);
+        for(int i = 0; i < keysCount; i++)
+            inventory.keys.Add(Resources.Load<GameObject>(NextString()));
+
+        int othersCount = NextInt();
+        inventory.others = new List<GameObject>(othersCount);
+        for(int i = 0; i < othersCount; i++)
+            inventory.others.Add(Resources.Load<GameObject>(NextString()));
+    }
+
+    protected void SavePlayerAttack()
+    {
+        var attack = gameObject.GetComponent<PlayerAttack>();
+        Put(attack.index);
+    }
+    protected void LoadPlayerAttack()
+    {
+        var attack = gameObject.GetComponent<PlayerAttack>();
+        if (attack == null) attack = gameObject.AddComponent<PlayerAttack>();
+
+        attack.index = NextInt();
+    }
+
+    protected void SavePlayerCastSpell()
+    {
+        var castSpell = gameObject.GetComponent<PlayerCastSpell>();
+        Put(castSpell.index);
+    }
+    protected void LoadPlayerCastSpell()
+    {
+        var castSpell = gameObject.GetComponent<PlayerCastSpell>();
+        if (castSpell == null) castSpell = gameObject.AddComponent<PlayerCastSpell>();
+        castSpell.index = NextInt();
+    }
+    
 }
